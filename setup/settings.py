15f7bddb1cf2f8060b183a2681ad205d15d469be
Alyne Perez <alyne.kmp@gmail.com>
2024-04-30 19:26:06 -0300

    feat: handling the static files

--- setup/settings.py
+++ setup/settings.py
@@ -124,4 +124,10 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "setup/static")    
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
